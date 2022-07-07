b003426410c55faceb087b29f41fabdd1eed2cf0
UgbolueWilliams <ugboluewilliams@yahoo.com>
2022-07-07 15:18:17 +0100

    Memory Game

---
+++ index.js
@@ -0,0 +1,7 @@
+const allBoxes=document.querySelectorAll('.container-items')
+const allImages=document.querySelecetorAll('img')
+
+const num=[6,10,10,2,4,11,13,1,8]
+const sorted=num.sort()
+console.log(sorted)
+
